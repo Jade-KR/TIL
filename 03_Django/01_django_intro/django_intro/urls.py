16aadf28db054c09fecc384c658d960b30af4d6f
Jade-kr <ijung1@naver.com>
2019-08-06 17:39:54 +0900

    0806

--- 03_Django/01_django_intro/django_intro/urls.py
+++ 03_Django/01_django_intro/django_intro/urls.py
@@ -18,6 +18,17 @@ from django.urls import path
 from pages import views
 
 urlpatterns = [
+    path('user_create/', views.user_create),
+    path('user_new/', views.user_new),
+    path('result/', views.result),
+    path('art/', views.art),
+    path('pong/', views.pong),
+    path('ping/', views.ping),
+    path('catch/', views.catch),
+    path('throw/', views.throw),
+    path('lotto/', views.lotto),
+    path('ispal/<char>/', views.ispal),
+    path('isbirth/<int:month>/<int:day>/', views.isbirth),
     path('template_language/', views.template_language),
     path('area/<int:r>/', views.area),
     path('prac2/<int:num1>/<int:num2>/', views.prac2),
--- 03_Django/01_django_intro/pages/views.py
+++ 03_Django/01_django_intro/pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from datetime import datetime
 import random
+import requests
 
 # 1. 기본로직
 # Create your views here.
@@ -73,3 +74,108 @@ def template_language(request):
     }
 
     return render(request, 'template_language.html', context)
+
+#6. 실습
+#6-1. isbirth
+def isbirth(request, month, day):
+    today = datetime.now()
+    if today.month == month and today.day == day:
+        result = True
+    else:
+        result = False
+    
+    context = {
+        'result': result
+    }
+
+    return render(request, 'isbirth.html', context)
+
+#6-2. 회문판별(palindrome)
+def ispal(request, char):
+    if char == char[::-1]:
+        result = True
+    else:
+        result = False
+
+    context = {
+        'result': result
+    }
+
+    return render(request, 'ispal.html', context)
+
+#6-3. 로또 번호 추첨
+# lottos -> 1 ~ 45까지의 번호 중 6개를 랜덤으로 pick한 리스트
+# real_lottos -> [21, 24, 30, 32, 40, 42]
+#1. lottos 번호를 하나씩 출력(DTL-for문)
+#2. 컴시기가 뽑은 로또 번호와 실제 로또 당첨 번호를 비교해보기(DTL-if문)
+def lotto(request):
+    real_lottos = [21, 24, 30, 32, 40, 42]
+    lottos = sorted(list(random.sample(range(1, 46), 6)))
+
+    context = {
+        'real_lottos': real_lottos,
+        'lottos': lottos,
+    }
+    return render(request, 'lotto.html', context)
+
+#7. Form - GET
+def throw(request):
+    return render(request, 'throw.html')
+
+def catch(request):
+    message = request.GET.get('message')
+    message2 = request.GET.get('message2')
+    context = {
+        'message': message,
+        'message2': message2,
+    }
+    return render(request, 'catch.html', context)
+
+def ping(request):
+    return render(request, 'ping.html') #주소창에 Enter 치는 순간 부터 어떤 일이 벌어질지 순차적으로 확인
+
+def pong(request):
+    ball = request.GET.get('ball')
+    ball2 = request.GET.get('ball2')
+    context = {
+        'ball': ball,
+        'ball2': ball2,
+    }
+    return render(request, 'pong.html', context)
+
+#8. Form - GET 실습(아스키 아티)
+def art(request):
+    return render(request, 'art.html')
+
+def result(request):
+    #1. form 으로 날린 데이터를 받는다.(GET)
+    word = request.GET.get('word')
+    #2. ARTII api로 요청을 보내 응답 결과를 fonts에 저장한다.
+    fonts = requests.get('http://artii.herokuapp.com/fonts_list').text
+    
+    #3. fonts(str)를 fonts(list)로 바꾼다.
+    fonts = fonts.split('\n')
+
+    #4. fonts(list)안에 들어있는 요소 중 하나를 선택해서 font라는 변수에 저장(str)
+    font = random.choice(fonts)
+
+    #5. 위에서 사용자에게 받은 word와 font를 가지고 다시 요청을 보낸다.
+    # 그리고 응답 결과를 result에 저장한다.
+    result = requests.get(f'http://artii.herokuapp.com/make?text={word}&font={font}').text
+    context = {
+        'result': result,
+    }
+    return render(request, 'result.html', context)
+
+#9. Form - POST (주소창에 나타나지 않고 바디로 보내줌)
+def user_new(request):
+    return render(request, 'user_new.html')
+
+def user_create(request):
+    name = request.POST.get('name')
+    pwd = request.POST.get('pwd')
+    context = {
+        'name': name,
+        'pwd': pwd,
+    }
+    return render(request, 'user_create.html', context)
