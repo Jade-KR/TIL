45ab49cb83fb710eb0cd220ddc4095cdf1699377
Jade <kjd8765@gmail.com>
2019-09-05 01:23:09 +0900

    0904

--- 04_algo/보충/0904/생일.py
+++ 04_algo/보충/0904/생일.py
@@ -7,21 +7,32 @@ for tc in range(1, T+1):
     rs = [list(map(int, input().split())) for _ in range(M)]
     check = [0]*(M+1)
     cnt = 0
-    tmp = []
+    tmp = [1]
+
     for i in range(M):
         if rs[i][0] == 1:
-            tmp.append(rs[i][1])
             check[rs[i][1]] = 1
-            cnt += 1
-
+            tmp.append(rs[i][1])
         elif rs[i][1] == 1:
-            tmp.append(rs[i][0])
             check[rs[i][0]] = 1
-            cnt += 1
+            tmp.append(rs[i][0])
 
     for i in range(M):
-        if rs[i][0] in tmp or rs[i][1] in tmp:
-            if check[rs[i][0]] != 0 and check[rs[i][1]] != 0:
-                cnt += 1
+        if rs[i][0] in tmp and rs[i][1] in tmp:
+            continue
+
+        if rs[i][0] in tmp and rs[i][1] not in tmp:
+            check[rs[i][1]] = 1
+            continue
+
+        if rs[i][1] in tmp and rs[i][0] not in tmp:
+            check[rs[i][0]] = 1
+            continue
+
+    for i in check:
+        if i == 1:
+            cnt += 1
 
     print('#{} {}'.format(tc, cnt))
+
+    # print('#{} {}'.format(tc, cnt))
