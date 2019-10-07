216d68ae277d043180620285c1f216ec4633b551
Jade-KR <kjd8765@gmail.com>
2019-10-07 17:33:23 +0900

    1007

--- 03_Django/04_django_crud_review/articles/views.py
+++ 03_Django/04_django_crud_review/articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.core.exceptions import ValidationError
 from .models import Article
+from IPython import embed
 
 def index(request):
     # articles = Article.objects.all()[::-1]
@@ -10,30 +11,39 @@ def index(request):
         }
     return render(request, 'articles/index.html', context)
 
-def new(request):
-    return render(request, 'articles/new.html')
-
 def create(request):
-    try:
+    #POST 요청일때
+    if request.method == 'POST':
         title = request.POST.get('title')
         content = request.POST.get('content')
         article = Article(title=title, content=content)
-        article.full_clean()
-    except ValidationError:
-        raise ValidationError('Your Error Message')
-    else:
         article.save()
         return redirect('articles:detail', article.pk)
+    #GET 요청일때
+    else:
+        return render(request, 'articles/create.html')
+
+    # try:
+    #     title = request.POST.get('title')
+    #     content = request.POST.get('content')
+    #     article = Article(title=title, content=content)
+    #     article.full_clean()
+    # except ValidationError:
+    #     raise ValidationError('Your Error Message')
+    # else:
+    #     article.save()
+    #     return redirect('articles:detail', article.pk)
+    
     '''
     title = request.POST.get('title')
     content = request.POST.get('content')
 
 
     #1. 첫번째 방법
-    # article = Article()
-    # article.title = title
-    # article.content = content
-    # article.save()
+    article = Article()
+    article.title = title
+    article.content = content
+    article.save()
 
     #2. 두번째 방법
     article = Article(title=title, content=content)
@@ -50,26 +60,29 @@ def detail(request, pk):
     }
     return render(request, 'articles/detail.html', context)
 
-def delete(reqeust, pk):
+def delete(request, pk):
     article = Article.objects.get(pk=pk)
-    article.delete()
-    return redirect('articles:index')
-
-def edit(request, pk):
-    article = Article.objects.get(pk=pk)
-    context = {
-        'article': article,
-    }
-    return render(request, 'articles/edit.html', context)
-
+    if request.method == 'POST':
+        article.delete()
+        return redirect('articles:index')
+    else:
+        return redirect('articles:detail', article.pk)
 
 def update(request, pk):
     article = Article.objects.get(pk=pk)
-    article.title = request.POST.get('title')
-    article.content = request.POST.get('content')
-    article.save()
+    if request.method == 'POST':
+        article.title = request.POST.get('title')
+        article.content = request.POST.get('content')
+        article.save()
+        return redirect('articles:detail', article.pk)
 
-    return redirect('articles:detail', article.pk)
+    else:
+        context = {
+        'article': article,
+        }
+        return render(request, 'articles/update.html', context)
+
+    
 
     # try:
     #     title = request.POST.get('title')
