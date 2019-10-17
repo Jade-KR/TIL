0273385508fe622eb0b660057d9a06f589f09db5
Jade-KR <kjd8765@gmail.com>
2019-10-17 18:01:57 +0900

    1017

--- 03_Django/06_django_form/articles/views.py
+++ 03_Django/06_django_form/articles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Article
+from django.views.decorators.http import require_POST
+from .models import Article, Comment
 from .forms import ArticleForm, CommentForm
 from IPython import embed
 
@@ -37,26 +38,20 @@ def create(request):
 
 def detail(request, article_pk): # urls 파일에 따라서 인자 이름 정하면 됨.
     article = get_object_or_404(Article, pk=article_pk)
-    if request.method == 'POST':
-        comment_form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = comment_form.save()
-            return redirect('articles:detail', article.pk)
-    else:
-        comment_form = CommentForm()
-        context = {
-            'article': article,
-            'comment_form': comment_form,
-        }
-        return render(request, 'articles/detail.html', context)
+    comments = article.comment_set.all()
+    comment_form = CommentForm()
+    context = {
+        'article': article,
+        'comment_form': comment_form,
+        'comments': comments,
+    }
+    return render(request, 'articles/detail.html', context)
 
+@require_POST
 def delete(request, article_pk):
     article = get_object_or_404(Article, pk=article_pk)
-    if request.method == 'POST':
-        article.delete()
-        return redirect('articles:index') # redirect -> GET 요청
-    else:
-        return redirect('articles:detail', article.pk)
+    article.delete()
+    return redirect('articles:index') # redirect -> GET 요청
 
 def update(request, article_pk):
     article = get_object_or_404(Article, pk=article_pk)
@@ -89,11 +84,18 @@ Update 로직
 - 수정된 글을 DB에 저장
 """
 
+@require_POST
 def comments_create(request, article_pk):
-    article = get_object_or_404(Article, pk=article_pk)
-    if request.method == 'POST':
-        comment_form = CommentForm(request.POST)
-        comment = comment_form.save(commit=False)
-        comment.article = article
+    # article = get_object_or_404(Article, pk=article_pk)
+    comment_form = CommentForm(request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False) # 아직 DB에 반영안됨.
+        comment.article_id = article_pk
         comment.save()
-    return redirect('articles:detail', article.pk)
+    return redirect('articles:detail', article_pk)
+
+@require_POST
+def comments_delete(request, article_pk, comment_pk):
+    comment = get_object_or_404(Comment, pk=comment_pk)
+    comment.delete()
+    return redirect('articles:detail', article_pk)
