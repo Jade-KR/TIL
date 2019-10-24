605d4cb4bf1d2cf797d5a6caf2ecab524e89bc5f
Jade-KR <kjd8765@gmail.com>
2019-10-24 18:33:08 +0900

    1024

--- 03_Django/06_django_form/articles/admin.py
+++ 03_Django/06_django_form/articles/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Article, Comment
+from .models import Article, Comment, Hashtag
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
@@ -13,3 +13,7 @@ class CommentAdmin(admin.ModelAdmin):
     list_display = ('pk', 'content', 'created_at', 'updated_at',)
 
 # admin.site.register(Comment, CommentAdmin)
+
+@admin.register(Hashtag)
+class HashtagAdmin(admin.ModelAdmin):
+    list_display = ('content', )
