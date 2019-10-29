9a0b7b787908b24006f942ded7e10515aa6aa0a3
Jade-KR <kjd8765@gmail.com>
2019-10-29 19:56:00 +0900

    1029

---
+++ 06_JS/00_js_intro/01_hoisting.js
@@ -0,0 +1,12 @@
+// Hoisting!
+// 나중에 선언된 변수를 참조 할 수 있음
+// 함수 or statement 최상단으로 올려지는 것 (hoisting)
+// 변수와 함수를 위한 메모리를 확보하는 과정
+
+// console.log(a)
+// var a = 10
+// console.log(a)
+
+console.log(b)
+let b = 10
+console.log(b)
