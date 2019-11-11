5664cfa4e6bab9ce4a732f3a86f031acfcc12707
Jade-KR <kjd8765@gmail.com>
2019-11-11 17:46:23 +0900

    1111

--- 04_algo/weakness/prac.py
+++ 04_algo/weakness/prac.py
@@ -1,22 +1,94 @@
 import sys
 sys.stdin = open('prac.txt')
+def dfs(x, y):
+    global chk
+    chk = 0
+    for k in range(4):
+        nx = x + dx[k]
+        ny = y + dy[k]
+        if data[nx][ny]:
+            chk = 1
+def bfs(x, y):
+    global check, cnt, result
+    q = []
+    visited[x][y] += 1
+    q.append((x, y))
+    while len(q) != 0:
+        x, y = q.pop(0)
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
 
-def dfs(v):
-    visited[v] = 1
-    for w in range(1, V+1):
-        if adj[v][w] == 1 and visited[v][w] == 0:
+            if nx < 0 or nx >= N or ny < 0 or ny >= M:
+                continue
+            elif visited[nx][ny] != 0:
+                continue
+            elif visited[nx][ny] == 0 and data[nx][ny] == 0:
+                visited[x][y] += 1
+                continue
+            elif visited[nx][ny] == 0 and data[nx][ny] != 0:
+                visited[nx][ny] += 1
+                q.append((nx, ny))
+
+    cnt += 1
 
 
-T = int(input())
-for tc in range(1, T+1):
-    V, E = map(int, input().split())
-    info = [list(map(int, input().split())) for _ in range(E)]
-    S, G = map(int, input().split())
-    adj = [[0]*(V+1) for _ in range(V+1)]
-    visited = [[0]*(V+1) for _ in range(V+1)]
 
-    for i in range(E):
-        adj[info[i][0]][info[i][1]] = 1
+    for n in range(N):
+        for m in range(M):
+            if data[n][m] > 0:
+                data[n][m] -= visited[n][m] - 1
+            if data[n][m] < 0:
+                data[n][m] = 0
 
-    dfs(S)
+    c1 = 0
 
+    for n in range(N):
+        for m in range(M):
+            if data[n][m]:
+                c1 += 1
+    if c1 == 0:
+        result = 0
+        check = 0
+
+N, M = map(int, input().split())
+data = [list(map(int, input().split())) for _ in range(N)]
+visited = [[0]*M for _ in range(N)]
+cnt = 0
+flag = 0
+check = 1
+x = 0
+y = 0
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+
+while check:
+    for i in range(N):
+        if flag == 1:
+            break
+        for j in range(M):
+            if data[i][j] != 0:
+                x = i
+                y = j
+                flag = 1
+                break
+    visited = [[0] * M for _ in range(N)]
+    bfs(x, y)
+    flag = 0
+
+    for n in range(N):
+        if flag == 1:
+            break
+        for m in range(M):
+            if data[n][m]:
+                dfs(n, m)
+                if chk == 0:
+                    check = 0
+                    result = cnt
+                    flag = 1
+                    break
+    flag = 0
+
+print(result)
---
+++ 07_Vue/04_vue_youtube_thumbnail_project/youtube-browser/src/components/VideoList.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul class="col-lg-4 list-group">
+    <video-list-item v-for="video in videos" 
+    :key="video.etag" 
+    :video="video"
+    @videoSelect="onVideoSelect"
+    >
+    </video-list-item>
+  </ul>
+</template>
+
+<script>
+import VideoListItem from "./VideoListItem"
+
+export default {
+  name: 'VideoList',
+  components: {
+    VideoListItem,
+  },
+  // props: ['videos'], // 내려 올 데이터가 몇개인지 알수 없기 떄문에 배열로 받음
+  props: {
+    videos: {
+      type: Array,
+      required: true,
+    }
+  },
+  methods: {
+    onVideoSelect(video) {
+      this.$emit('videoSelect', video)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
