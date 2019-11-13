5b490a68905b948ee403cf8c2b95a9b110863dbd
Jade-KR <kjd8765@gmail.com>
2019-11-14 01:19:08 +0900

    1114

--- 04_algo/보충/1113/게리맨더링.py
+++ 04_algo/보충/1113/게리맨더링.py
@@ -1,30 +1,27 @@
 import sys
-import collections
 sys.stdin = open('게리맨더링.txt')
 
-def bfs(w):
-    global check, flag
-    flag = 0
-    check = []
-    check.append(w)
-    visited = [0] * (N + 1)
-    q = collections.deque()
-    q.append(w)
+def dfs(w):
+    global flag, cnt, check
     visited[w] = 1
-    while q:
-        t = q.popleft()
-        for o in range(1, N+1):
-            if visited[o] == 1 or adj[t][o] == 0:
-                continue
-            visited[o] = 1
-            q.append(o)
-            check.append(o)
-
-    if check in g1:
+    for k in range(1, N+1):
+        if adj[w][k] == 0 or visited[k] == 1:
+            continue
+        visited[k] = 1
+        dfs(k)
+    if flag == 1:
+        return
+    cnt = 0
+    for i in range(N+1):
+        if visited[i] == 1:
+            cnt += 1
+    if cnt == N+1:
         flag = 1
-
+        check = 1
+        return
 
 def powerset(n, dep):
+    global visited, flag, cnt, check, result, validator
     if n == dep:
         g1 = []
         g2 = []
@@ -33,15 +30,45 @@ def powerset(n, dep):
                 g1.append(arr[i])
             else:
                 g2.append(arr[i])
-        print(g1)
-        print(g2)
-        print()
-        if g1:
-            for i in g1:
-                bfs(i)
-        if g2:
-            for j in g2:
-                bfs(j)
+        # validator = 0
+        # for i in g1:
+        #     for j in range(1, info[i-1][0]+1):
+        #         if info[i-1][j] in g2:
+        #             validator = 1
+        #
+        # if validator == 1:
+        if len(g1) != 0 and len(g1) != N:
+            if g1:
+                visited = [1] * (N + 1)
+                flag = 0
+                cnt = 0
+                check = 0
+                for i in range(len(g1)):
+                    visited[g1[i]] = 0
+                dfs(g1[i])
+
+                if check == 1:
+                    if g2:
+                        visited = [1] * (N + 1)
+                        flag = 0
+                        cnt = 0
+                        check = 0
+                        for i in range(len(g2)):
+                            visited[g2[i]] = 0
+                        dfs(g2[i])
+
+
+                        if check == 1:
+                            sum_g1 = 0
+                            sum_g2 = 0
+                            for p in g1:
+                                sum_g1 += people[p-1]
+                            for p in g2:
+                                sum_g2 += people[p-1]
+                            tmp = abs(sum_g1-sum_g2)
+                            if result > tmp:
+                                result = tmp
+
     else:
         A[dep] = 1
         powerset(n, dep+1)
@@ -53,8 +80,10 @@ N = int(input())
 people = list(map(int, input().split()))
 info = [list(map(int, input().split())) for _ in range(N)]
 adj = [[0] * (N+1) for _ in range(N+1)]
-visited = [[0] * (N+1) for _ in range(N+1)]
-
+cnt = 0
+check = 0
+result = 98765124
+validator = 0
 for i in range(N):
     for j in range(1, len(info[i])):
         adj[i+1][info[i][j]] = 1
@@ -62,4 +91,8 @@ for i in range(N):
 arr = [i+1 for i in range(N)]
 A = [0] * N
 
-powerset(6, 0)
+powerset(N, 0)
+
+if result == 98765124:
+    result = -1
+print(result)
