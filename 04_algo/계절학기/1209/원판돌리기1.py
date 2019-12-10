b94817efd76628e64669a44f532dac446c1858e4
Jade-KR <kjd8765@gmail.com>
2019-12-11 08:55:52 +0900

    study

--- 04_algo/계절학기/1209/원판돌리기1.py
+++ 04_algo/계절학기/1209/원판돌리기1.py
@@ -5,10 +5,7 @@ sys.stdin = open('원판돌리기.txt')
 N, M, T = map(int, input().split())
 circle = [list(map(int, input().split())) for _ in range(N)]
 info = [list(map(int, input().split())) for _ in range(T)]
-check = 0
-sum_c = 0
 ans = 0
-cnt = 0
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
@@ -25,29 +22,21 @@ for i in range(T):
         # 인접한 것들 중 같은 값 체크
         for a in range(N):
             for b in range(M):
-                if circle[a][b] != 0:
+                if circle[a][b] != -1000:
                     for k in range(4):
                         na = a + dx[k]
                         nb = b + dy[k]
-                        if na < 0 or na >= N or nb < 0 or nb >= M: continue
+                        if na < 0 or na >= N or nb >= M: continue
                         if circle[a][b] == circle[na][nb]:
                             visited[a][b] = 1
                             visited[na][nb] = 1
-                    if b == 0:
-                        if circle[a][b] == circle[a][b - 1]:
-                            visited[a][b] = 1
-                            visited[a][b - 1] = 1
-
-                        if circle[a][b] == circle[a][b + 1]:
-                            visited[a][b] = 1
-                            visited[a][b + 1] = 1
 
         check = 0
         # 인접한 값이 같은 것을 지울때
         for i in range(N):
             for j in range(M):
                 if visited[i][j]:
-                    circle[i][j] = 0
+                    circle[i][j] = -1000
                     check = 1
         # 인접할 값을 지울 수 없을때
         if check == 0:
@@ -56,7 +45,7 @@ for i in range(T):
             c = 0
             for i in range(N):
                 for j in range(M):
-                    if circle[i][j] == 0:
+                    if circle[i][j] == -1000:
                         cnt += 1
                     else:
                         sum_c += circle[i][j]
@@ -68,10 +57,12 @@ for i in range(T):
                 #평균 보다 크거나 작은 값 +- 1
                 for h in range(N):
                     for k in range(M):
-                        if circle[h][k] > avg and circle[h][k] != 0:
+                        if circle[h][k] > avg and circle[h][k] != -1000:
                             circle[h][k] -= 1
-                        elif circle[h][k] < avg and circle[h][k] != 0:
+                        elif circle[h][k] < avg and circle[h][k] != -1000:
                             circle[h][k] += 1
+            else:
+                break
 
     elif info[i][1] == 1:
         # 원판 돌리기 (반시계방향)
@@ -84,29 +75,21 @@ for i in range(T):
         # 인접한 것들 중 같은 값 체크
         for a in range(N):
             for b in range(M):
-                if circle[a][b] != 0:
+                if circle[a][b] != -1000:
                     for k in range(4):
                         na = a + dx[k]
                         nb = b + dy[k]
-                        if na < 0 or na >= N or nb < 0 or nb >= M: continue
+                        if na < 0 or na >= N or nb >= M: continue
                         if circle[a][b] == circle[na][nb]:
                             visited[a][b] = 1
                             visited[na][nb] = 1
-                    if b == 0:
-                        if circle[a][b] == circle[a][b - 1]:
-                            visited[a][b] = 1
-                            visited[a][b - 1] = 1
-
-                        if circle[a][b] == circle[a][b + 1]:
-                            visited[a][b] = 1
-                            visited[a][b + 1] = 1
 
         check = 0
         # 인접한 값이 같은 것을 지울 때
         for i in range(N):
             for j in range(M):
                 if visited[i][j]:
-                    circle[i][j] = 0
+                    circle[i][j] = -1000
                     check = 1
 
         # 인접한 값을 지울 것이 없을 때
@@ -116,7 +99,7 @@ for i in range(T):
             c = 0
             for i in range(N):
                 for j in range(M):
-                    if circle[i][j] == 0:
+                    if circle[i][j] == -1000:
                         cnt += 1
                     else:
                         sum_c += circle[i][j]
@@ -128,13 +111,16 @@ for i in range(T):
                 # 평균 보다 크거나 작은 값 +- 1
                 for h in range(N):
                     for k in range(M):
-                        if circle[h][k] > avg and circle[h][k] != 0:
+                        if circle[h][k] > avg and circle[h][k] != -1000:
                             circle[h][k] -= 1
-                        elif circle[h][k] < avg and circle[h][k] != 0:
+                        elif circle[h][k] < avg and circle[h][k] != -1000:
                             circle[h][k] += 1
+            else:
+                break
 
 for i in range(N):
     for j in range(M):
-        ans += circle[i][j]
+        if circle[i][j] > 0:
+            ans += circle[i][j]
 
 print(ans)
