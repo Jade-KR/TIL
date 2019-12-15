6d20095081c1a30ab695ff8d0efb989b636964e9
Jade-KR <kjd8765@gmail.com>
2019-12-16 01:38:14 +0900

    study

--- 04_algo/계절학기/1213/인구이동.py
+++ 04_algo/계절학기/1213/인구이동.py
@@ -3,48 +3,68 @@ sys.stdin = open("인구이동.txt")
 import collections
 
 def bfs(x, y):
+    global chk
     q = collections.deque()
-    visited[i][j] = 1
+    visited[x][y] = 1
     q.append((x, y))
     while len(q):
-        global check
         x, y = q.popleft()
         for k in range(4):
             nx = x + dx[k]
             ny = y + dy[k]
             if nx < 0 or nx >= N or ny < 0 or ny >= N: continue
             if visited[nx][ny] != 0: continue
-            visited[nx][ny] = 1
-            if L <= abs(population[x][y] - population[nx][ny]) <= R:
-                visited[nx][ny] = 2
-                visited[x][y] = 2
-            q.append((nx, ny))
+            if L <= abs(population[nx][ny] - population[x][y]) <= R:
+                share[nx][ny] = alians
+                share[x][y] = alians
+                visited[nx][ny] = 1
+                q.append((nx, ny))
+                chk = 1
 
-    check = 1
-    for a in range(N):
-        for b in range(N):
-            if visited[a][b] == 2:
-                check = 0
 
 N, L, R = map(int, input().split())
 population = [list(map(int, input().split())) for _ in range(N)]
-visited = [[0]*N for _ in range(N)]
 check = 0
+ans = -1
+chk = 0
 
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
-while check==0:
+
+while check == 0:
+    alians = 1
+    cnt = 0
+    check = 1
     visited = [[0] * N for _ in range(N)]
+    share = [[0] * N for _ in range(N)]
     for i in range(N):
         for j in range(N):
             if visited[i][j] == 0:
                 bfs(i, j)
-    cnt = 0
-    tmp = 0
-    for i in range(N):
-        for j in range(N):
-            if visited[i][j] == 2:
-                tmp += population[i][j]
-                cnt += 1
-    avg = int(tmp/cnt)
+                if chk == 1:
+                    alians += 1
+                    chk = 0
+                    check = 0
+
+    ans += 1
+    for c in range(1, alians):
+        tmp = 0
+        avg = 0
+        cnt = 0
+        for a in range(N):
+            for b in range(N):
+                if share[a][b] == c:
+                    cnt += 1
+                    tmp += population[a][b]
+
+
+        if cnt != 0:
+            avg = int(tmp / cnt)
+            for a in range(N):
+                for b in range(N):
+                    if share[a][b] == c:
+                        population[a][b] = avg
+        else:
+            check = 1
+print(ans)
